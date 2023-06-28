4faaa7edf621f189bee926a8f9f8553209e8b34a
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-06-28 12:32:41 +0600

    hello

--- index.js
+++ index.js
@@ -4,24 +4,26 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
-// Mongo DB Connections
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, {
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-     serverApi: ServerApiVersion.v1
-});
-
-
 // Middleware Connections
 app.use(cors())
 app.use(express.json())
 
 
+
+// Mongo DB Connections
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iybyxmu.mongodb.net`;
+const client = new MongoClient(uri);
+
 // Routes
 async function run() {
      try {
+          const appointmentOptionCollection = client.db("dentist_portal").collection("appointmentOptions");
+
+          app.get("/appointmentOptions", async (req, res) => {
+               const query = {};
+               const options = await appointmentOptionCollection.find(query).toArray();
+               res.send(options);
+          })
 
      } finally {
 
@@ -31,14 +33,12 @@ async function run() {
 run().catch(console.log)
 
 app.get('/', (req, res) => {
-
-     res.send('hello from dentist server :)')
-
-})
+     res.send('hello from dentist server :)');
+});
 
 
 // Connection
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-     console.log('App running in port: ' + PORT)
-})
+     console.log('App running in port: ' + PORT);
+});
