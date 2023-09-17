ef44d9885036c931b1ef4134051a9164f0935af8
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-17 10:17:59 +0600

    pas

--- index.js
+++ index.js
@@ -8,22 +8,24 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+const PORT = process.env.PORT || 5000;
+
 
 
 // Mongo DB Connections
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iybyxmu.mongodb.net`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iybyxmu.mongodb.net/`;
 const client = new MongoClient(uri);
 
 // Routes
 async function run() {
      try {
-          const appointmentOptionCollection = client.db("dentist_portal").collection("appointmentOptions");
+          const appointmentOptionCollection = client.db('dentist_portal').collection('appointmentOptions');
 
           app.get("/appointmentOptions", async (req, res) => {
                const query = {};
                const options = await appointmentOptionCollection.find(query).toArray();
                res.send(options);
-          })
+          });
 
      } finally {
 
@@ -36,9 +38,8 @@ app.get('/', (req, res) => {
      res.send('hello from dentist server :)');
 });
 
-
 // Connection
-const PORT = process.env.PORT || 5000
+
 app.listen(PORT, () => {
      console.log('App running in port: ' + PORT);
 });
