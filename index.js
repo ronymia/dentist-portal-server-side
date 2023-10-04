723c336fb6d275cbbbc237bfa4e5374d2fccfb89
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-10-04 16:24:50 +0600

    admin api

--- index.js
+++ index.js
@@ -73,6 +73,19 @@ async function run() {
                return res.send(result);
           })
 
+          app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+               const email = req.params;
+               // CHECKING ADMIN
+               if (req.decoded.email !== email) {
+                    return res.send({ admin: false });
+               }
+
+               const query = { email: email };
+               const user = await userCollection.findOne(query);
+               const result = { admin: user?.role === 'admin' };
+               res.send(result);
+          })
+
           app.patch(("/users/admin/:id"), async (req, res) => {
                const id = req.params.id;
                const filter = { _id: new ObjectId(id) };
